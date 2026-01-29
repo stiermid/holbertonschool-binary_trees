180c385f17c1a3e7511b28a26fed41812c6b8087
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-29 11:13:25 +0400

    fix: task0
    * Simplicity always works

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,19 +12,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (parent->left != NULL && parent->right != NULL)
-		return (NULL);
-
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->left != NULL)
-		parent->right = new_node;
-	else
-		parent->left = new_node;
-
 	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 	new_node->n = value;
 
 	return (new_node);
