413b4ef8f72539c74536f09a70ee95abe10a58a6
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-31 00:25:21 +0400

    feat: task11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the size
+ *
+ * Return: the size of the binary tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
