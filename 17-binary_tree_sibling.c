65b5a44b2d254017b5846f6915e6e4f1876c12ee
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-31 16:43:31 +0400

    feat: task17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: a pointer to the node to find the sibling
+ *
+ * Return: a pointer to the sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	if (node->parent->right == node)
+		return (node->parent->left);
+
+	return (NULL);
+}
+
